7ceeb616320bbd63dfcdc028df036e84e4e791d1
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-20 16:18:58 -0500

    Update allystats10.asm

--- disasm/data/stats/allies/stats/allystats10.asm
+++ disasm/data/stats/allies/stats/allystats10.asm
@@ -3,22 +3,24 @@
 ; 0x1EE4DC..0x1EE4FE : Ally stats 10
 AllyStats10:    forClass  BDMN ; luke
                 hpGrowth  12, 48, LINEAR
-                mpGrowth  2, 6, EARLY
-                attGrowth 8, 44, LINEAR
-                defGrowth 6, 35, LINEAR
+                mpGrowth  2, 56, EARLY
+                attGrowth 8, 54, LINEAR
+                defGrowth 6, 50, LINEAR
                 agiGrowth 7, 50, LINEAR
                 spellList &
-                    1, HYPER_HEAL, &
-                    15, BLAST, &
-                    22, BLAST|LV2, &                    
-                    35, BLAST|LV3, &
-                    55, BLAST|LV4
+                    1, HEALIN|LV4, &
+                    2,BOOST|LV3,&
+                    3,RIPSAW|LV2,&
+                    10, BLAST, &
+                    15, BLAST|LV2, &                    
+                    25, BLAST|LV3, &
+                    35, BLAST|LV4
                     
                 forClass  BDBT
                 hpGrowth  48, 120, LATE
-                mpGrowth  6, 26, EARLY
-                attGrowth 44, 84, LINEAR
-                defGrowth 35, 70, LINEAR
-                agiGrowth 50, 87, LINEAR
+                mpGrowth  6, 126, EARLY
+                attGrowth 44, 184, LINEAR
+                defGrowth 35, 170, LINEAR
+                agiGrowth 50, 187, LINEAR
                 useFirstSpellList
                     
