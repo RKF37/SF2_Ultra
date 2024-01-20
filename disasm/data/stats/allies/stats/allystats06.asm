51918d548153f83fd99584e94de3e525003ec90d
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-20 15:49:32 -0500

    Update allystats06.asm

--- disasm/data/stats/allies/stats/allystats06.asm
+++ disasm/data/stats/allies/stats/allystats06.asm
@@ -3,26 +3,28 @@
 ; 0x1EE454..0x1EE476 : Ally stats 06
 AllyStats06:    forClass  MAGG ;NATASHA
                 hpGrowth  15, 35, LATE
-                mpGrowth  7, 40, EARLY
+                mpGrowth  7, 55, EARLY
                 attGrowth 4, 24, LINEAR
                 defGrowth 4, 24, LINEAR
                 agiGrowth 6, 42, LINEAR
                 spellList &
                     1, FREEZE, &
-                    9, FREEZE|LV2, &
+                    3, FREEZE|LV2, &
+                    5,FREEZE|LV3,&
+                    10,FREEZE|LV4,&
                     11, BLAST, &
                     14, BOLT, &
                     18, BLAST|LV2, &
                     19, BOLT|LV2, &
                     31, BOLT|LV3, &
-                    33, SLEEP, &
+                    33, PYTHON|LV3, &
                     37, BLAST|LV3, &
                     49, BOLT|LV4, &
                     51, BLAST|LV4
                     
                 forClass  WIZZ
-                hpGrowth  35, 93, LATE
-                mpGrowth  40, 95, LINEAR
+                hpGrowth  35, 193, LATE
+                mpGrowth  40, 195, LINEAR
                 attGrowth 24, 48, LINEAR
                 defGrowth 24, 62, LINEAR
                 agiGrowth 42, 83, EARLYANDLATE
