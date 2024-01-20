9ca186136a8df322dafccfa64513ef58feea355e
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-20 14:23:58 -0500

    Update allystats01.asm

--- disasm/data/stats/allies/stats/allystats01.asm
+++ disasm/data/stats/allies/stats/allystats01.asm
@@ -4,14 +4,14 @@
 AllyStats01:    forClass  PRST
                 hpGrowth  12, 52, LINEAR
                 mpGrowth  0, 45, EARLY
-                attGrowth 6, 26, LINEAR
-                defGrowth 4, 25, LINEAR
-                agiGrowth 5, 31, LINEAR
+                attGrowth 6, 46, LINEAR
+                defGrowth 4, 46, LINEAR
+                agiGrowth 5, 46, LINEAR
                 spellList &
-                    2, HEAL, &
-                    10, HEAL|LV2, &
-                    23, HEAL|LV3, &
-                    42, HEAL|LV4, &
+                    2, AURA, &
+                    10, AURA|LV2, &
+                    23, AURA|LV3, &
+                    30, AURA|LV4, &
                     4, DETOX, &
                     14, DETOX|LV2, &
                     34, DETOX|LV3, &
@@ -20,10 +20,9 @@ AllyStats01:    forClass  PRST
                     17, ATOM|LV2, &
                     26, ATOM|LV3, &                    
                     45, ATOM|LV4, &
-                    12, AURA, &
-                    28, AURA|LV2, &
-                    29,AURA|LV3,&
-					30 AURA|LV4
+                    12,PYTHON|LV3
+                    
+                    
                 forClass  VICR
                 hpGrowth  52, 115, LINEAR
                 mpGrowth  24, 70, LINEAR
@@ -33,10 +32,10 @@ AllyStats01:    forClass  PRST
                 useFirstSpellList
                     
                 forClass  MMNK
-                hpGrowth  52, 105, LINEAR
-                mpGrowth  24, 50, LINEAR
-                attGrowth 40, 80, LINEAR
-                defGrowth 25, 70, LATE
-                agiGrowth 31, 61, MIDDLE
+                hpGrowth  52, 165, LINEAR
+                mpGrowth  24, 195, LINEAR
+                attGrowth 40, 180, LINEAR
+                defGrowth 25, 170, LATE
+                agiGrowth 31, 161, MIDDLE
                 useFirstSpellList
                     
