838a109e70253420ff982d94d13d94b9596ac874
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-01-20 16:32:54 -0500

    Update allystats17.asm

--- disasm/data/stats/allies/stats/allystats17.asm
+++ disasm/data/stats/allies/stats/allystats17.asm
@@ -3,24 +3,24 @@
 ; 0x1EE628..0x1EE685 : Ally stats 17
 AllyStats17:    forClass  WITC   ; ANRI
                 hpGrowth  40, 90, LINEAR
-                mpGrowth  50, 100, EARLY
-                attGrowth 30, 70, LINEAR
-                defGrowth 31, 50, LINEAR
-                agiGrowth 33, 45, LINEAR
+                mpGrowth  50, 200, EARLY
+                attGrowth 30, 170, LINEAR
+                defGrowth 31, 150, LINEAR
+                agiGrowth 33, 145, LINEAR
                 spellList &
                      1, FREEZE, &
                     11, FREEZE|LV2, &
                     20, FREEZE|LV3, &
-                    50, FREEZE|LV4, &
+                    30, FREEZE|LV4, &
                     5, RAIJIN, &
                     15, RAIJIN|LV2, &
-                    37, RAIJIN|LV3, &
+                    37, RAIJIN|LV4, &
                     29, BEAM, &
-                    32, BEAM|LV2, &
-                    23, APOLLO, &
-                    35, APOLLO|LV2, &
-                    33, SLEEP, &
-                    44, BEAM|LV3, &
-                    45, APOLLO|LV3, &
-                    60, BEAM|LV4					
+                    32, BEAM|LV4, &
+                    23, PYTHON|LV3
+                    
+                    
+                  
+                    
+                  				
                 
