81448dccab9c08efd1c0082edbee7ffeaaf58864
RKF37 <155844380+RKF37@users.noreply.github.com>
2024-06-27 22:49:26 -0400

    Update allystats30.asm

--- disasm/data/stats/allies/stats/allystats30.asm
+++ disasm/data/stats/allies/stats/allystats30.asm
@@ -3,7 +3,7 @@
 ; 0x1EE31C..0x1EE36B : Ally stats 30
 AllyStats30:    forClass  ACHH ; SHADE
                 hpGrowth  8, 44, LATE
-                mpGrowth  0,50, NONE
+                mpGrowth  0,50, EARLY
                 attGrowth 6, 46, LINEAR
                 defGrowth 5,45, LATE
                 agiGrowth 6,50, EARLY
